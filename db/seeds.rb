7f8f6b775bcdd5912f90d10f29509b909c8fc6ca
Alison Wood <alisonwood29@gmail.com>
2017-12-15 14:42:26 +0000

    added save method for category and seeded db

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+require_relative("../models/category")
+require_relative("../models/vendor")
+require_relative("../models/transaction")
+require("pry-byebug")
+
+category_1 = Category.new({ "name" => "Eating Out"})
+category_2 = Category.new({ "name" => "Groceries"})
+category_3 = Category.new({ "name" => "Social"})
+category_4 = Category.new({ "name" => "Clothes"})
+category_5 = Category.new({ "name" => "House"})
+category_6 = Category.new({ "name" => "Car"})
+
+category_1.save()
+category_2.save()
+category_3.save()
+category_4.save()
+category_5.save()
+category_6.save()
+
+binding.pry
+nil
--- models/category.rb
+++ models/category.rb
@@ -10,6 +10,17 @@ class Category
     @name = options["name"]
   end
 
+  def save()
+    sql = "INSERT INTO categories (name)
+          VALUES ($1) RETURNING id"
+    values = [@name]
+    category_hash = SqlRunner.run(sql, values).first
+    @id = category_hash["id"].to_i
+  end
+
+  def Category.all()
+    
+  end
 
 
 end
