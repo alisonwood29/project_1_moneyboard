752fc7b593eb4da7b82c056be69963a8bb24a6ae
Alison Wood <alisonwood29@gmail.com>
2017-12-15 13:03:32 +0000

    initial commit: created directories and created tables and sql runner

---
+++ db/moneyboard.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS vendors;
+
+
+CREATE TABLE vendors(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE categories(
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE transactions(
+  id SERIAL4 PRIMARY KEY,
+  category_id INT4 REFERENCES categories(id),
+  vendor_id INT4 REFERENCES vendors(id),
+  amount INT2
+);
