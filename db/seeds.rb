8877a521e8b280dd514190bf69f593ac717fddcb
Alison Wood <alisonwood29@gmail.com>
2017-12-17 18:09:42 +0000

    added all and delete all methods to vendor and seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@ require_relative("../models/transaction")
 require("pry-byebug")
 
 Category.delete_all()
+Vendor.delete_all()
 
 category_1 = Category.new({ "name" => "Eating Out"})
 category_2 = Category.new({ "name" => "Groceries"})
@@ -18,6 +19,16 @@ category_3.save()
 category_4.save()
 category_5.save()
 category_6.save()
+
+vendor_1 = Vendor.new({"name" => "Nandos"})
+vendor_2 = Vendor.new({"name" => "Tesco"})
+vendor_3 = Vendor.new({"name" => "Topshop"})
+vendor_4 = Vendor.new({"name" => "Chanter"})
+
+vendor_1.save()
+vendor_2.save()
+vendor_3.save()
+vendor_4.save()
 
 
 
--- models/vendor.rb
+++ models/vendor.rb
@@ -16,4 +16,15 @@ class Vendor
     vendor_hash = SqlRunner.run(sql, values).first
     @id = vendor_hash["id"].to_i
   end
+
+  def Vendor.all()
+    sql = "SELECT * FROM vendors;"
+    vendor_hashes = SqlRunner.run(sql)
+    return vendor_hashes.map{|vendor| Vendor.new(vendor)}
+  end
+
+  def Vendor.delete_all()
+    sql = "DELETE FROM vendors;"
+    SqlRunner.run(sql)
+  end
 end
