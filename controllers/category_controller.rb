643c19f70604d05c847dfe15eeed7eedfb87dd2e
Alison Wood <alisonwood29@gmail.com>
2017-12-18 14:32:25 +0000

    added new.erb to category controller

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -8,3 +8,13 @@ get("/categories") do
   @categories = Category.all()
   erb(:"categories/index")
 end
+
+get("/categories/new") do
+  erb(:"categories/new")
+end
+
+post("/categories") do
+  @category = Category.new(params)
+  @category.save()
+  redirect "/categories"
+end
